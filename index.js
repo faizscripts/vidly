6502167b3ed6584fe2c92379272497294bdae641
Faiz Ahmed <4faizahmed@gmail.com>
2021-03-13 11:56:04 +0300

    Error handling

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+require('express-async-errors');
+const winston = require('winston');
+require('winston-mongodb');
 const config = require('config');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
 const mongoose = require('mongoose');
 const express = require('express');
+const app = express();
 const home = require('./routes/home')
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
@@ -10,18 +14,33 @@ const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
 const users = require('./routes/users');
 const auth = require('./routes/auth');
+const error = require('./middleware/error');
+
+winston.exceptions.handle(new winston.transports.File({filename: 'uncaughtExceptions.log'}));
+
+
+process.on('unhandledRejection', error => {
+    throw Error(error);
+});
+
+
+winston.add(new winston.transports.File({filename: 'logfile.log'}));
+winston.add(new winston.transports.MongoDB({db: 'mongodb://localhost/vidly', level: 'warn'}));
+
+// throw new Error('synchronous error');
+//
+// const p = Promise.reject(new Error('asynchronous error'));
+// p.then(value => console.log('Done'));
 
 if (!config.get('jwtPrivateKey')) {
     console.error('FATAL ERROR: jwtPrivateKey is not defined');
     process.exit(1);
 }
 
-mongoose.connect('mongodb://localhost/vidly', {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect('mongodb://localhost/vidly', {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true})
     .then(()=> console.log('Connected to Vidly successfully...'))
     .catch(reason => console.log(`Unable to connect to Vidly!!! ${reason}`));
 
-
-const app = express();
 
 app.use(express.json());
 app.use('/', home);
@@ -32,7 +51,7 @@ app.use('/rentals', rentals);
 app.use('/users', users);
 app.use('/auth', auth);
 
-
+app.use(error);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
